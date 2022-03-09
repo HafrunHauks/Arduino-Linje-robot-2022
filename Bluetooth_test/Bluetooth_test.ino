c163c68f1d3dbfd9adc87c55c23c50d252e6b811
henryeikeland <101245341+henryeikeland@users.noreply.github.com>
2022-03-09 08:48:26 +0100

    Create Bluetooth_test.ino

---
+++ Bluetooth_test/Bluetooth_test.ino
@@ -0,0 +1,34 @@
+
+#include <SoftwareSerial.h>         //inkludere bibliotek til serial avlesing for porter som ikke er TX/RX
+SoftwareSerial mySerial(11, 10);     //definere port 10 og 9 til serial avlesing RX/TX
+#include <Wire.h>
+
+
+char val;                           //variabel for tekst
+char seriell_tekst;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);               //start seriell avlesning
+  mySerial.begin(9600);             //start bluetooth seriell avlesning
+  Wire.begin();                     //start I2C kommuniserings prosess
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (mySerial.available()) {                           //hvis tekst blir detektert på myserial portene
+    val = mySerial.read();                              //variabel for tekst leser av myserial
+    Serial.print(val);                                  //variabel for tekst printer innholdet i lokalt seriell vindu.
+  }
+
+if  (Serial.available()) {   
+  seriell_tekst = Serial.read();
+  mySerial.write(seriell_tekst);
+}
+
+
+//Serial.print (1);
+//delay(500);
+}
